c09ac7b4fe93770ab31012602dedc777d9cafc1b
Eric McKinney <mckinney99@gmail.com>
2017-12-14 18:50:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinzer' do
+    p = PigLatinizer.new 
+    @pigged = p.piglatinzer(params[:user_phrase])
+    
+  end
 end
