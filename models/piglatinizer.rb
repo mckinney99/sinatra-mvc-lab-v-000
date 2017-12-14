db7452de1a74a8b89c6ea705b78a03bb7d76f6bd
Eric McKinney <mckinney99@gmail.com>
2017-12-14 20:21:18 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,18 @@
+class PigLatinizer
+
+  def piglatinize(text)
+    text.split.map do |word|
+      if /\A[aeiou]/i.match(word)
+        "#{word}way"
+      else
+        cons = word.split(/[aeiou]/).first
+        "#{word.sub(cons,"")}#{cons}ay"
+      end
+      end.join(' ')
+    end
+
+  def to_pig_latin(text)
+    piglatinize(text)
+    end
+
+end
