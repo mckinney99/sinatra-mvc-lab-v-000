9976486f72f20bd5b85ab3e53b2b022c444b2a26
Eric McKinney <mckinney99@gmail.com>
2017-12-14 19:11:08 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,3 +5,29 @@ class PigLatinizer
   end
 
   
+  def to_pig_latin(text)
+    words = text.split(" ").collect do |word|
+      alphabet = ('a'..'z').to_a
+      vowels = ["a", "e", "i", "o", "u"]
+      consonants = alphabet - vowels
+
+      if vowels.include?(word[0].downcase)
+        word + 'way'
+      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase) && consonants.include?(word[2].downcase)
+        first, second = word[0..2], word[3..-1]
+        second + first + "ay"
+      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase)
+        first, second = word[0..1], word[2..-1]
+        second + first + "ay"
+      elsif consonants.include?(word[0].downcase)
+        first, second = word[0], word[1..-1]
+        second + first + "ay"
+      else
+        word
+      end
+    end
+
+    words.join(" ")
+  end
+
+
