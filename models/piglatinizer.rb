ad1917f3073d017ae481cc4fc87a3f9a61cd3457
Eric McKinney <mckinney99@gmail.com>
2017-12-14 19:57:20 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,16 +1,16 @@
 class PigLatinizer
 
   def initialize
- +  end
- +
- +  def piglatinize(string)
- +    words = string.split(" ").collect do |word|
- +      if word[0].match(/[aeiou]/i)
- +        word += "way"
- +      else shift = word.split(/[aeiou]/).first
- +        word[(shift.length)..-1] + shift + "ay"
- +      end
- +    end
- +    words.join(" ")
- +  end
- +end
+   end
+
+  def piglatinize(string)
+    words = string.split(" ").collect do |word|
+      if word[0].match(/[aeiou]/i)
+        word += "way"
+      else shift = word.split(/[aeiou]/).first
+        word[(shift.length)..-1] + shift + "ay"
+      end
+    end
+    words.join(" ")
+  end
+end
