8fcd479b4d8dfe4dc9baae17108660293fdef1e5
Eric McKinney <mckinney99@gmail.com>
2017-12-14 19:56:23 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,33 +1,16 @@
 class PigLatinizer
 
-  def piglatinize(text)
-    pigged_words = to_pig_latin(text)
-  end
-
-
-  def to_pig_latin(text)
-    words = text.split(" ").collect do |word|
-      alphabet = ('a'..'z').to_a
-      vowels = ["a", "e", "i", "o", "u"]
-      consonants = alphabet - vowels
-
-      if vowels.include?(word[0].downcase)
-        word + 'way'
-      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase) && consonants.include?(word[2].downcase)
-        first, second = word[0..2], word[3..-1]
-        second + first + "ay"
-      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase)
-        first, second = word[0..1], word[2..-1]
-        second + first + "ay"
-      elsif consonants.include?(word[0].downcase)
-        first, second = word[0], word[1..-1]
-        second + first + "ay"
-      else
-        words
-      end
-    end
-
-
-    words.join(" ")
-  end
-end
+  def initialize
+ +  end
+ +
+ +  def piglatinize(string)
+ +    words = string.split(" ").collect do |word|
+ +      if word[0].match(/[aeiou]/i)
+ +        word += "way"
+ +      else shift = word.split(/[aeiou]/).first
+ +        word[(shift.length)..-1] + shift + "ay"
+ +      end
+ +    end
+ +    words.join(" ")
+ +  end
+ +end
